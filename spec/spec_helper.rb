0c0b57bbb1cfe3445a70b6f8aafcdd88366387de
ProGM <progiemmeh@gmail.com>
2021-04-01 11:09:44 +0200

    :fix: add frozen_string_literal: true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'active_support'
 require 'rspec'
 require 'ecs_deploy_cli'
-# require 'kaminari-activerecord'
 
 I18n.enforce_available_locales = false
 RSpec::Expectations.configuration.warn_about_potential_false_positives = false
@@ -11,5 +12,3 @@ Dir[File.expand_path('../support/*.rb', __FILE__)].each { |f| require f }
 RSpec.configure do |config|
 
 end
-
-
